e87dbe590745fc38332eae15b0f16e7cdf5cdc8c
danil <danil@DESKTOP-T4KQBOA>
2026-06-02 11:37:42 +0500

    проверка и запрос разрешений. закурин

--- app/src/main/java/com/example/rp11_zakurin/MainActivity.java
+++ app/src/main/java/com/example/rp11_zakurin/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.rp11_zakurin;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -47,4 +51,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    public Boolean GetPermissionGPS(){
+        ACCESS_FINE_LOCATION = ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION);
+        ACCESS_COARSE_LOCATION = ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION);
+        return ACCESS_FINE_LOCATION == PackageManager.PERMISSION_GRANTED ||
+                ACCESS_COARSE_LOCATION == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public void OnGetGPS(View view){
+        if(GetPermissionGPS() == false){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
 }
